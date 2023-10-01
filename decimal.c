a6a4c101fa9571f9e73c1fef6097e20048ebce46
Anish Gupta <anishgupta675@gmail.com>
2023-10-01 21:04:04 +0530

    added IEEE 754 standard type lib

---
+++ decimal.c
@@ -0,0 +1,26 @@
+#include "test.h"
+void deci1()
+{
+ signed int a = 10;
+ except(-90, a-100);
+
+}
+void deci2()
+{
+    int arr[5] = {7,3,9,5,1};
+    *ar = arr + 1;
+    except(3, ar);
+}
+void deci3(){
+    int x , y, z;
+    x = (10 > 4)?10 + 4 : 10 - 4;
+    y = x * 6;
+    z = y % X;
+    except(196 , pow(x , 2));
+}
+int main()
+{
+    deci1();
+    deci2();
+    deci3();
+}
