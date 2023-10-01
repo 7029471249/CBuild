59135f22336b96d232496b1c4f355e1c98b13119
Anish Gupta <anishgupta675@gmail.com>
2023-10-01 21:04:53 +0530

    added package lib

---
+++ import.c
@@ -0,0 +1,17 @@
+#include "test.h"
+
+#import "import.h"
+#import "import.h"
+#include "import.h"
+
+#import "some.h"
+#include "some.h"
+#import "some.h"
+
+void main()
+{
+    print("import");
+
+    print("some");
+    
+}
