7b878afcbea79920bd9ff75cc9bac75f3d00df54
Anish Gupta <anishgupta675@gmail.com>
2023-10-01 21:05:07 +0530

    added standard type lib

---
+++ int.c
@@ -0,0 +1,33 @@
+#include "test.h"
+
+
+void main()
+{
+short int i = 3 ;
+short i1 = 5;
+accepts(8, i + i1);
+accepts(15, i * i1);
+
+ signed short int i2 = 28 ;
+ signed short i3 = 14 ;
+accepts(42, i2 + i3);
+accepts(392 , i2 * i3);
+
+long int l = 55555555 ;
+long l1 = 11111111 ;
+acceptl(66666666, l + l1);
+acceptl(44444444, l - l1);
+
+ signed long int l2 = 300 ;
+acceptl();
+
+unsigned long int l3 =  ;
+unsigned long int l3 =  ;
+acceptl();
+
+unsigned int u =  ;
+signed int s =  ;
+accept();
+accept();
+
+}
