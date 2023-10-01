0f9db061694c8b4552d35c24473bbbedf8359692
Anish Gupta <anishgupta675@gmail.com>
2023-10-01 21:03:35 +0530

    added ref lib

---
+++ constexpr.c
@@ -0,0 +1,10 @@
+#include "test.h"
+static const num = 34;
+int *fun = &num;
+int fun2 =  (198 < pow(14,2)) ? 14 : 2;
+
+void main(){
+print("Constant Expression");
+accept(34 , num);
+accept(2 , fun2);
+}
