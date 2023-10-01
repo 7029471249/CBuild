d528e830c6eb241a60bd8cb068e059a40b9f0108
Anish Gupta <anishgupta675@gmail.com>
2023-10-01 21:04:56 +0530

    added package lib

---
+++ import.h
@@ -0,0 +1,14 @@
+#ifdef IMPORT
+#error "#import location error "
+#endif
+
+#ifdef IMPORT_H
+#error "#import location error"
+#endif
+
+#ifdef IMPORT_C
+#error "#import location error"
+#endif
+
+#define IMPORT_H1
+#define IMPORT_C1
