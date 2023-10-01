31699e1cf254dfee826800301a32f99aa2478f47
Anish Gupta <anishgupta675@gmail.com>
2023-10-01 21:04:21 +0530

    added custom type lib

---
+++ enum.c
@@ -0,0 +1,17 @@
+#include "test.h"
+enum week{sunday , monday , tuesday , wednesday , thursday , friday , saturday};
+//enum values by default starts from 0
+enum planets{Mars , Sun, Neptune , Venus , Moon}
+int main()
+{
+    print("enumerator");
+    enum week day , planets name;
+    day = friday;
+    accept(5 , day);
+    accept(0 , sunday);
+    acccept(1 , monday);
+    accept(4 , thursday);
+    name = Neptune;
+    accept(2 , name);
+    
+}
